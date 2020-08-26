1bc67b68d6599c9333d560ffdcbb9ccf6ca8e202
Lei0911 <lzhu51@my.bcit.ca>
2020-08-26 12:14:18 -0700

    edit auth/google/callback link

--- config/passport_google.js
+++ config/passport_google.js
@@ -9,8 +9,7 @@ module.exports = (passport) => {
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL:
-                    "http://lei-website.herokuapp.com/auth/google/callback",
+                callbackURL: "/auth/google/callback",
             },
             async (accessToken, refreshToken, profile, done) => {
                 // console.log(profile);
