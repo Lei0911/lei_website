4426de698aea2b4b5893f1e870b6a1a58defefbc
Lei0911 <lzhu51@my.bcit.ca>
2020-09-10 15:11:42 -0700

    edit api page under construction

--- public/javascript/APIsHome.js
+++ public/javascript/APIsHome.js
@@ -2,6 +2,6 @@ const movieAPI = document.getElementById("movieAPI");
 const gameAPI = document.getElementById("gameAPI");
 const hidden_notice = document.getElementsByClassName("hidden_notice");
 
-movieAPI.addEventListener("click", () => {
-    window.location.href = "movieAPIHome";
-});
+// movieAPI.addEventListener("click", () => {
+//     window.location.href = "movieAPIHome";
+// });
